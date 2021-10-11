38fccc1db3052ae294a2971c21db8f1a1c2666cb
Nasa1423 <40059434+Nasa1423@users.noreply.github.com>
2021-10-11 15:14:32 +0300

    Test comments added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
 
+# class GameField
+
 class GameField:
     def __init__(self):
         self.height, self.width = 50, 50
@@ -10,6 +12,11 @@ class Bones:
     def __init__(self):
         self.boneA = random.randint(1, 6)
         self.boneB = random.randint(1, 6)
+
+    # method throw
+    #return:
+    #   self.boneA : Integer => Value of the first dropped bone
+    #   self.boneB : Integer => Value of the second dropped bone
     def throw(self):
         self.boneA, self.boneB = random.randint(1, 6), random.randint(1, 6)
         return self.boneA, self.boneB
