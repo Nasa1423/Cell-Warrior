3792a1b9a8613e4210f9399fd91ef871137567a8
Nasa1423 <40059434+Nasa1423@users.noreply.github.com>
2021-10-11 15:11:07 +0300

    Bones added

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import random
+
 class GameField:
     def __init__(self):
-        height, width = 50, 50
-        cells = [[0 for _ in range(width)] for _ in range(height)]
+        self.height, self.width = 50, 50
+        self.cells = [[0 for _ in range(self.width)] for _ in range(self.height)]
+
+
+class Bones:
+    def __init__(self):
+        self.boneA = random.randint(1, 6)
+        self.boneB = random.randint(1, 6)
+    def throw(self):
+        self.boneA, self.boneB = random.randint(1, 6), random.randint(1, 6)
+        return self.boneA, self.boneB
