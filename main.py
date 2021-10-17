c1fea0283f83e911a189f03933234dc1a35c785d
evreuushkaa <pafaktorovich@edu.hse.ru>
2021-10-17 16:14:18 +0300

    Базовая загрузка

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import random
+import pygame
+import sys
+import os
+pygame.init()
 
-# class GameField
+class Game:
+    def __init__(self, height, width):
+        window = pygame.display.set_mode((1280, 720)) #окно игры
+        pygame.display.set_caption("Cell-Warrior") #название окна --> наша игра
+        self.field = GameField(height, width)
+        self.bones = Bones()
+        #Отрисовка главного меню
+    def turn(self):
+        a, b = self.bones.throw()
+        #Переотрисовка
 
-class GameField:
-    def __init__(self):
-        self.height, self.width = 50, 50
+class GameField: #игровое поле, все, что его касается#
+    def __init__(self, height, width):
+        self.height, self.width = height, width
         self.cells = [[0 for _ in range(self.width)] for _ in range(self.height)]
 
 
-class Bones:
+class Bones:#кости
     def __init__(self):
         self.boneA = random.randint(1, 6)
         self.boneB = random.randint(1, 6)
