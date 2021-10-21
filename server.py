f72d3a3461bff5f132f2df6cf0af52a13cc6eeff
lbogova <vrbogova@miem.hse.ru>
2021-10-22 01:16:07 +0300

    1

--- server.py
+++ server.py
@@ -1,3 +1,29 @@
+import socketio
+
 class Server:
     def __init__(self):
-        pass
+
+        sio = socketio.AsyncServer()
+        app = socketio.ASGIApp(sio)
+
+        @sio.event
+        async def connect(sid, environ):
+            print(sid, 'connected')
+
+        @sio.event
+        async def disconnect(sid):
+            print(sid, 'disconnected')
+
+
+class Client:
+    def __init__(self):
+
+        sio = socketio.AsyncClient()
+
+        @sio.event
+        async def message(data):
+            print('recieved')
+
+        @sio.on('*')
+        async def catch_all(event, sid, data):
+            pass
