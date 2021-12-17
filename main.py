146401810449ba6b955718bccbfdf90641a4a4ef
evreuushkaa <pafaktorovich@edu.hse.ru>
2021-12-17 13:54:25 +0300

    добавили фиксацию прямоугольников

--- main.py
+++ main.py
@@ -20,37 +20,39 @@ class Game:
         pygame.display.set_caption("Cell-Warrior") #название окна --> наша игра
         pygame.draw.rect(self.window, WHITE, (20, 20, 500, 500))
         self.size = 500 / width
+        h_coordinates = []
+        v_coordinates = []
         #print(self.size)
         for horizon in range(1, width):
             first = 20 + horizon * (self.size)
             pygame.draw.line(self.window, BLACK, [first, 20], [first, 520])
+            h_coordinates.append(first)
             pygame.display.update()
         for vertical in range(1, width):
             second = 20 + vertical * (self.size)
             pygame.draw.line(self.window, BLACK, [0, second], [520, second])
+            v_coordinates.append(second)
             pygame.display.update()
         field_pos = pygame.Rect(20, 20, 500, 500)
         save_field = self.window.subsurface(field_pos)
         pygame.image.save(save_field, "our_field.jpg")
 
-        self.draw_rect(2, 2, 3, 3, GREEN)
+        self.draw_rect(2, 2, 4, 3, GREEN)
         self.field = GameField(width, width)
         self.bones = Bones()
         self.turn()
-
-
         #Отрисовка главного меню
+
         #Отрисовка прямоугольников
     def draw_rect(self, x0, y0, width, height, color):
         #print(20 + (x0 - 1) * self.size)
-        pygame.draw.rect(self.window, color, (21 + (x0 - 1) * self.size, 21 + (y0 - 1) * self.size, width * self.size - 1, height * self.size - 1))
+        pygame.draw.rect(self.window, color, (21 + (x0 - 1) * self.size, 21 + (y0 - 1) * self.size, width * self.size, height * self.size - 1))
         pygame.display.update()
 
     #отрисовка поля
     def draw_cell(self, width):
         field = pygame.sprite.Group()
         field.add("our_field.jpg")
-
 
     def turn(self):
         a, b = self.bones.throw()
@@ -68,10 +70,7 @@ class Game:
                         self.draw_cell(30)
                         if pygame.mouse.get_focused():
                             pygame.draw.rect(
-                                self.window, GREEN, (x - 10,
-                                           y - 10,
-                                           20, 20))
-
+                                self.window, GREEN, (x - 10, y - 10, 20, 20))
                         pygame.display.update()
 
 class Objects(pygame.sprite.Sprite):
@@ -84,13 +83,12 @@ class Objects(pygame.sprite.Sprite):
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, player):
         pygame.sprite.Sprite.__init__(self)
-
         self.image = pygame.image.load(player)
         self.image = pygame.transform.scale(self.image, (300, 75))
-
         startPoint = (100, 500)
         self.rect = self.image.get_rect()
         self.rect.bottomleft = (startPoint)
         #Переотрисовка
+
 test = Game(30)
 test.turn()
