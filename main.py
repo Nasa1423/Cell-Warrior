7c1e0f1e1d54376d852d1df936af20a6a880f1cb
evreuushkaa <pafaktorovich@edu.hse.ru>
2021-12-18 00:58:36 +0300

    Добавили второго игрока и поменяли цвет его фигур.

--- main.py
+++ main.py
@@ -44,8 +44,10 @@ class Game:
         self.rect_dr.add(self.field_our)
         self.game_manager = GameField(width, width)
         self.bones = Bones()
+        self.turn_num = 1
         while True:
             self.turn()
+            self.turn_num = 2 if self.turn_num == 1 else 1
 
     def turn(self):
         """
@@ -56,14 +58,13 @@ class Game:
         """
         a, b = self.bones.throw()
         x, y = 0, 0
-        positions = self.game_manager.getAvalablePositions(Square(x, y, a, b, 1))
+        positions = self.game_manager.getAvalablePositions(Square(x, y, a, b, self.turn_num))
         positions = [x.getCoords() for x in positions[0]]
-        coordinates = [[((w - x) / 2 + x + 2) * self.size, ((h - y) / 2 + y + 2) * self.size] for x, y, w, h in positions]
-        sel_positions = -1
         print(positions)
-        self.rect = Rect_drawing(self.size * a, self.size * b, GREEN)
+        coordinates = [[((w - x) / 2 + x + 1) * self.size, ((h - y) / 2 + y + 1) * self.size] for x, y, w, h in positions]
+        sel_positions = -1
+        self.rect = Rect_drawing(self.size * a, self.size * b, GREEN) if self.turn_num == 1 else Rect_drawing(self.size * a, self.size * b, PINK)
         self.rect_dr.add(self.rect)
-        print(type(self.rect))
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -75,7 +76,6 @@ class Game:
                     if 520 - 15 > x > 20 + 15 and 20 + 15 < y < 520 - 15:
                         self.window.fill(BLACK)
                         if pygame.mouse.get_focused():
-                            massive = []
                             coord_x, coord_y = 500, 500
                             minimum_dist = math.hypot(500, 500)
                             for posit in range(len(coordinates)):
@@ -90,8 +90,7 @@ class Game:
                     if 520 > x > 20 and 20 < y < 520:
                         self.window.fill(BLACK)
                         if pygame.mouse.get_focused():
-                            f = self.game_manager.addSquare(positions[sel_positions][0], positions[sel_positions][1], a, b, 1)
-                            print(f)
+                            self.game_manager.addSquare(positions[sel_positions][0], positions[sel_positions][1], a, b, self.turn_num)
                             return
                 self.rect_dr.update()
                 self.rect_dr.draw(self.window)
@@ -105,12 +104,9 @@ class FieldImage(pygame.sprite.Sprite):
 
     def __init__(self, image):
         pygame.sprite.Sprite.__init__(self)
-        # field_pos = pygame.Rect(20, 20, 500, 500)
-        # self.image = image
         self.image = pygame.image.load("our_field.jpg").convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.bottomleft = ((20, 520))
-        # self.image = pygame.image.load("our_field.jpg").convert_alpha()
 
 
 class Rect_drawing(pygame.sprite.Sprite):
@@ -154,4 +150,3 @@ class Rect_drawing(pygame.sprite.Sprite):
 
 
 test = Game(25)
-# test.turn()
---
+++ test_main.py
@@ -0,0 +1,10 @@
+import unittest
+
+import pygame, pygame.transform
+
+from pygame.tests.test_utils import question
+
+from pygame import display
+
+class maintests(unittest.TestCase):
+
